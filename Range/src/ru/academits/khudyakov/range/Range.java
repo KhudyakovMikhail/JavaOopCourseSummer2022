12cdeb1078a6d90b4cac2778192370e950f35502
Михаил <hudyakovmk@gmail.com>
2022-07-19 15:13:09 +0700

    Реализованы методы для задачи Range* Создан класс Range2_main для задачи Range*

--- Range/src/ru/academits/khudyakov/range/Range.java
+++ Range/src/ru/academits/khudyakov/range/Range.java
@@ -32,4 +32,100 @@ public class Range {
     public boolean isInside(double number) {
         return number >= from && number <= to;
     }
+
+    public Range getIntersection(Range range) {
+        if (this.from < range.from) {
+            if (this.to <= range.from) {
+                return null;
+            }
+
+            if (this.to <= range.to) {
+                return new Range(range.from, this.to);
+            }
+
+            return range;
+        }
+
+        if (range.from < this.from) {
+            if (range.to <= this.from) {
+                return null;
+            }
+
+            if (range.to <= this.to) {
+                return new Range(this.from, range.to);
+            }
+
+            return this;
+        }
+
+        if (this.to <= range.to) {
+            return this;
+        }
+
+        return new Range(this.from, range.to);
+    }
+
+    public Range[] getUnion(Range range) {
+        if (this.from < range.from) {
+            if (this.to < range.from) {
+                return new Range[]{this, range};
+            }
+
+            if (this.to < range.to) {
+                return new Range[]{new Range(this.from, range.to)};
+            }
+
+            return new Range[]{this};
+        }
+
+        if (range.from < this.from) {
+            if (range.to < this.from) {
+                return new Range[]{this, range};
+            }
+
+            if (range.to < this.to) {
+                return new Range[]{new Range(range.from, this.to)};
+            }
+
+            return new Range[]{range};
+        }
+
+        if (this.to < range.to) {
+            return new Range[]{new Range(this.from, range.to)};
+        }
+
+        return new Range[]{this};
+    }
+
+    public Range[] getDefinition(Range range) {
+        if (this.from < range.from) {
+            if (this.to <= range.from) {
+                return new Range[]{this};
+            }
+
+            if (this.to <= range.to) {
+                return new Range[]{new Range(this.from, range.from)};
+            }
+
+            return new Range[]{new Range(this.from, range.from), new Range(range.to, this.to)};
+        }
+
+        if (range.from < this.from) {
+            if (range.to <= this.from) {
+                return new Range[]{this};
+            }
+
+            if (range.to < this.to) {
+                return new Range[]{new Range(range.to, this.to)};
+            }
+
+            return null;
+        }
+
+        if (range.to < this.to) {
+            return new Range[]{new Range(range.to, this.to)};
+        }
+
+        return null;
+    }
 }
