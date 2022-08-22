ff1864163b4ec5e9f99476ab61917fcab02d0584
Михаил <hudyakovmk@gmail.com>
2022-08-22 11:19:25 +0700

    Внес исправления в задачу ArrayListHome

--- ArrayListHome/src/ru/academist/khudyakov/arraylist_home/ArrayListHome.java
+++ ArrayListHome/src/ru/academist/khudyakov/arraylist_home/ArrayListHome.java
@@ -7,19 +7,15 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayListHome {
-    public static ArrayList<String> getFileLines(String name) {
-        ArrayList<String> lines = new ArrayList<>();
-
-        try (Scanner scanner = new Scanner(new FileInputStream(name))) {
+    public static ArrayList<String> getFileLines(String fileName) throws FileNotFoundException {
+        try (Scanner scanner = new Scanner(new FileInputStream(fileName))) {
+            ArrayList<String> lines = new ArrayList<>();
             while (scanner.hasNextLine()) {
                 lines.add(scanner.nextLine());
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            System.out.println("Файл не найден");
-        }
 
-        return lines;
+            return lines;
+        }
     }
 
 
@@ -37,19 +33,24 @@ public class ArrayListHome {
 
     public static ArrayList<Integer> getListWithoutDuplicates(ArrayList<Integer> list) {
         ArrayList<Integer> listWithoutDuplicates = new ArrayList<>(list.size());
-        ArrayList<Integer> listCopy = new ArrayList<>(list);
 
-        while (!listCopy.isEmpty()) {
-            listWithoutDuplicates.add(listCopy.get(0));
-            listCopy.removeAll(listWithoutDuplicates);
+        for (Integer element : list) {
+            if (!listWithoutDuplicates.contains(element)) {
+                listWithoutDuplicates.add(element);
+            }
         }
 
         return listWithoutDuplicates;
     }
 
     public static void main(String[] args) {
-        ArrayList<String> fileStrings = getFileLines("ArrayListHomeInput.txt");
-        System.out.println(fileStrings);
+        try {
+            ArrayList<String> fileStrings = getFileLines("ArrayListHomeInput.txt");
+            System.out.println("Строки файла: " + fileStrings);
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден.");
+        }
+
         System.out.println();
 
         ArrayList<Integer> list = new ArrayList<>();
@@ -59,17 +60,17 @@ public class ArrayListHome {
         }
 
         deleteEvenNumbers(list);
-        System.out.println(list);
+        System.out.println("Список с удаленными четными числами: " + list);
 
         list = new ArrayList<>(Arrays.asList(1, 1, 2, 2, 5, 5, 6, 6, 7, 8, 9, 10, 15, 16, 16, 16, 16, 17));
 
         deleteEvenNumbers(list);
-        System.out.println(list);
+        System.out.println("Список с удаленными четными числами: " + list);
         System.out.println();
 
-        ArrayList<Integer> listWithDuplicates = new ArrayList<>(Arrays.asList(1, 2, 7, 4, 5, 4, 2, 3, 2, 4, 9, 7, 6, 4, 8, 3, 6, 2, 7, 9, 0, 1, 5, 2, 8, 5, 0, 3));
+        ArrayList<Integer> listWithDuplicates = new ArrayList<>(Arrays.asList(1, 1, 2, 7, 4, 5, 4, 2, 3, 2, 4, 9, 7, 6, 4, 8, 3, 6, 2, 7, 9, 0, 1, 5, 2, 8, 5, 0, 3));
 
         ArrayList<Integer> listWithoutDuplicates = getListWithoutDuplicates(listWithDuplicates);
-        System.out.println(listWithoutDuplicates);
+        System.out.println("Список без повторений: " + listWithoutDuplicates);
     }
 }
