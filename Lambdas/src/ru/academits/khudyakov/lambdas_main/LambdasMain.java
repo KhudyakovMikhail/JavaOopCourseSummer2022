5e69b0441e7ffd52219959ca7440fdd3fa1d4c48
Михаил <hudyakovmk@gmail.com>
2022-09-19 10:43:04 +0700

    Внес изменения в задачу "Лямбды"

--- Lambdas/src/ru/academits/khudyakov/lambdas_main/LambdasMain.java
+++ Lambdas/src/ru/academits/khudyakov/lambdas_main/LambdasMain.java
@@ -2,28 +2,25 @@ package ru.academits.khudyakov.lambdas_main;
 
 import ru.academits.khudyakov.lambdas.Person;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.OptionalDouble;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class LambdasMain {
     public static void main(String[] args) {
-        List<Person> persons = new ArrayList<>();
+        List<Person> persons = Arrays.asList(
+                new Person("Максим", 17),
+                new Person("Елена", 33),
+                new Person("Иван", 42),
+                new Person("Ольга", 26),
+                new Person("Дмитрий", 33),
+                new Person("Иван", 80),
+                new Person("Кристина", 39),
+                new Person("Даниил", 9),
+                new Person("Владислав", 65),
+                new Person("Кристина", 16)
+        );
 
-        persons.add(new Person("Максим", 17));
-        persons.add(new Person("Елена", 33));
-        persons.add(new Person("Иван", 42));
-        persons.add(new Person("Ольга", 26));
-        persons.add(new Person("Дмитрий", 33));
-        persons.add(new Person("Иван", 80));
-        persons.add(new Person("Кристина", 39));
-        persons.add(new Person("Даниил", 9));
-        persons.add(new Person("Владислав", 65));
-        persons.add(new Person("Кристина", 16));
-
-        // А
+        // А получить список уникальных имен
         List<String> uniqueNames = persons.stream()
                 .map(Person::getName)
                 .distinct()
@@ -31,17 +28,17 @@ public class LambdasMain {
 
         System.out.println("Список уникальных имен: " + uniqueNames);
 
-        // Б
-        String uniqueNamesString = persons.stream()
-                .map(Person::getName)
-                .distinct()
+        // Б вывести список уникальных имен в формате:
+        // Имена: Иван, Сергей, Петр.
+        String uniqueNamesString = uniqueNames.stream()
                 .collect(Collectors.joining(", ", "Имена: ", "."));
 
         System.out.println(uniqueNamesString);
 
-        // В
+        // B получить список людей младше 18, посчитать для них средний возраст
         List<Person> youngerThan18Peoples = persons.stream()
-                .filter(person -> person.getAge() < 18).toList();
+                .filter(person -> person.getAge() < 18)
+                .toList();
 
         System.out.println(youngerThan18Peoples);
 
@@ -51,16 +48,15 @@ public class LambdasMain {
 
         System.out.println(youngerThan18PeoplesAverageAge);
 
-        // Г
-        Map<String, Double> averageAgeByName = persons.stream()
+        // Г при помощи группировки получить Map, в котором ключи - имена, а значения - средний возраст
+        Map<String, Double> averageAgeByNames = persons.stream()
                 .collect(Collectors.groupingBy(Person::getName, Collectors.averagingInt(Person::getAge)));
 
-        System.out.println(averageAgeByName);
+        System.out.println(averageAgeByNames);
 
-        // Д
+        // Д получить людей, возраст которых от 20 до 45, вывести в консоль их имена в порядке убывания возраста
         persons.stream()
-                .filter(person -> person.getAge() > 20)
-                .filter(person -> person.getAge() < 45)
+                .filter(person -> person.getAge() >= 20 && person.getAge() <= 45)
                 .sorted((person1, person2) -> person2.getAge() - person1.getAge())
                 .forEach(System.out::println);
     }
