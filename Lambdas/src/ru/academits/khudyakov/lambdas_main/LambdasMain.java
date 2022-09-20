454114554268c2083943432471aff4d89703b394
Михаил <hudyakovmk@gmail.com>
2022-09-20 09:48:19 +0700

    Добавил пояснительный текст в задачу Лямбды

--- Lambdas/src/ru/academits/khudyakov/lambdas_main/LambdasMain.java
+++ Lambdas/src/ru/academits/khudyakov/lambdas_main/LambdasMain.java
@@ -26,35 +26,46 @@ public class LambdasMain {
                 .distinct()
                 .toList();
 
-        System.out.println("Список уникальных имен: " + uniqueNames);
+        System.out.println("Получить список уникальных имен:");
+        System.out.println(uniqueNames);
+        System.out.println();
 
         // Б вывести список уникальных имен в формате:
         // Имена: Иван, Сергей, Петр.
         String uniqueNamesString = uniqueNames.stream()
                 .collect(Collectors.joining(", ", "Имена: ", "."));
 
+        System.out.println("Вывести список уникальных имен в формате:");
         System.out.println(uniqueNamesString);
+        System.out.println();
 
         // B получить список людей младше 18, посчитать для них средний возраст
         List<Person> youngerThan18Peoples = persons.stream()
                 .filter(person -> person.getAge() < 18)
                 .toList();
 
+        System.out.println("Получить список людей младше 18:");
         System.out.println(youngerThan18Peoples);
 
         OptionalDouble youngerThan18PeoplesAverageAge = youngerThan18Peoples.stream()
                 .mapToInt(Person::getAge)
                 .average();
 
+        System.out.println("Посчитать для них средний возраст:");
         System.out.println(youngerThan18PeoplesAverageAge);
+        System.out.println();
 
         // Г при помощи группировки получить Map, в котором ключи - имена, а значения - средний возраст
         Map<String, Double> averageAgeByNames = persons.stream()
                 .collect(Collectors.groupingBy(Person::getName, Collectors.averagingInt(Person::getAge)));
 
+        System.out.println("При помощи группировки получить Map, в котором ключи - имена, а значения - средний возраст:");
         System.out.println(averageAgeByNames);
+        System.out.println();
 
         // Д получить людей, возраст которых от 20 до 45, вывести в консоль их имена в порядке убывания возраста
+        System.out.println("Получить людей, возраст которых от 20 до 45, вывести в консоль их имена в порядке убывания возраста:");
+
         persons.stream()
                 .filter(person -> person.getAge() >= 20 && person.getAge() <= 45)
                 .sorted((person1, person2) -> person2.getAge() - person1.getAge())
