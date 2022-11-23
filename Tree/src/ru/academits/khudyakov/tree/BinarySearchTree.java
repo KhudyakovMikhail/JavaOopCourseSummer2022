cd0ee07be819fa2b0a09f128c1a7c40c2d78c58e
Михаил <hudyakovmk@gmail.com>
2022-11-23 21:50:53 +0700

    Сделал метод удаления узла в задачу Tree

--- Tree/src/ru/academits/khudyakov/tree/BinarySearchTree.java
+++ Tree/src/ru/academits/khudyakov/tree/BinarySearchTree.java
@@ -58,12 +58,13 @@ public class BinarySearchTree<T> {
             TreeNode<T> newNode = new TreeNode<>(data);
             TreeNode<T> currentNode = root;
 
-            do {
+            while (true) {
                 if (compare(data, currentNode.getData()) < 0) {
                     if (currentNode.getLeft() != null) {
                         currentNode = currentNode.getLeft();
                         continue;
                     }
+
                     currentNode.setLeft(newNode);
                     size++;
                     break;
@@ -74,11 +75,12 @@ public class BinarySearchTree<T> {
                         currentNode = currentNode.getRight();
                         continue;
                     }
+
                     currentNode.setRight(newNode);
                     size++;
                     break;
                 }
-            } while (currentNode != null);
+            }
         }
     }
 
@@ -109,7 +111,120 @@ public class BinarySearchTree<T> {
     }
 
     public boolean remove(T data) {
-        return false;
+        if (root == null || !contains(data)) {
+            return false;
+        }
+
+        size--;
+
+        if (compare(data, root.getData()) == 0) {
+            if (root.getLeft() == null && root.getRight() == null) {
+                root = null;
+
+                return true;
+            }
+
+            if ((root.getLeft() == null && root.getRight() != null) || (root.getRight() == null && root.getLeft() != null)) {
+                root = (root.getLeft() == null) ? root.getRight() : root.getLeft();
+
+                return true;
+            }
+
+            TreeNode<T> currentItemParent = root.getRight();
+            TreeNode<T> currentItem = currentItemParent.getLeft();
+
+            while (currentItem.getLeft() != null) {
+                currentItem = currentItem.getLeft();
+                currentItemParent = currentItemParent.getLeft();
+            }
+
+            if (currentItem.getRight() != null) {
+                currentItemParent.setLeft(currentItem.getRight());
+            }
+
+            currentItem.setLeft(root.getLeft());
+            currentItem.setRight(root.getRight());
+            root = currentItem;
+        }
+
+        TreeNode<T> removedItemParent = root;
+        TreeNode<T> removedItem;
+
+        if (compare(data, root.getData()) < 0) {
+            removedItem = root.getLeft();
+        } else {
+            removedItem = root.getRight();
+        }
+
+        while (compare(data, removedItem.getData()) != 0) {
+            removedItemParent = removedItem;
+
+            if (compare(data, removedItem.getData()) < 0) {
+                removedItem = removedItem.getLeft();
+            } else {
+                removedItem = removedItem.getRight();
+            }
+        }
+
+        if (removedItem.getLeft() == null && removedItem.getRight() == null) {
+            if (compare(data, removedItemParent.getData()) < 0) {
+                removedItemParent.setLeft(null);
+            } else {
+                removedItemParent.setRight(null);
+            }
+
+            return true;
+        }
+
+        if ((removedItem.getLeft() == null && removedItem.getRight() != null) || (removedItem.getRight() == null && removedItem.getLeft() != null)) {
+            TreeNode<T> removedItemChild = (removedItem.getLeft() == null) ? removedItem.getRight() : removedItem.getLeft();
+
+            if (compare(data, removedItemParent.getData()) < 0) {
+                removedItemParent.setLeft(removedItemChild);
+            } else {
+                removedItemParent.setRight(removedItemChild);
+            }
+
+            return true;
+        }
+
+        TreeNode<T> minimumLeftChildParent = removedItem.getRight();
+        TreeNode<T> minimumLeftChild = minimumLeftChildParent.getLeft();
+
+        if (minimumLeftChild == null) {
+            minimumLeftChildParent.setRight(null);
+            minimumLeftChildParent.setLeft(removedItem.getLeft());
+
+            if (compare(data, removedItemParent.getData()) < 0) {
+                removedItemParent.setLeft(minimumLeftChildParent);
+            } else {
+                removedItemParent.setRight(minimumLeftChildParent);
+            }
+
+            return true;
+        }
+
+        while (minimumLeftChild.getLeft() != null) {
+            minimumLeftChildParent = minimumLeftChild;
+            minimumLeftChild = minimumLeftChild.getLeft();
+        }
+
+        if (minimumLeftChild.getRight() != null) {
+            minimumLeftChildParent.setLeft(minimumLeftChild.getRight());
+        } else {
+            minimumLeftChildParent.setLeft(null);
+        }
+
+        if (compare(data, removedItemParent.getData()) < 0) {
+            removedItemParent.setLeft(minimumLeftChild);
+        } else {
+            removedItemParent.setRight(minimumLeftChild);
+        }
+
+        minimumLeftChild.setLeft(removedItem.getLeft());
+        minimumLeftChild.setRight(removedItem.getRight());
+
+        return true;
     }
 
     public void traverseInBreadth(Consumer<T> consumer) {
--- Tree/src/ru/academits/khudyakov/tree_main/BinarySearchTreeMain.java
+++ Tree/src/ru/academits/khudyakov/tree_main/BinarySearchTreeMain.java
@@ -7,24 +7,18 @@ public class BinarySearchTreeMain {
         BinarySearchTree<Integer> binarySearchTree = new BinarySearchTree<>();
 
         binarySearchTree.add(30);
-        binarySearchTree.add(32);
-        binarySearchTree.add(18);
+        binarySearchTree.add(50);
+        binarySearchTree.add(15);
         binarySearchTree.add(10);
-        binarySearchTree.add(7);
-        binarySearchTree.add(46);
         binarySearchTree.add(35);
-        binarySearchTree.add(39);
-        binarySearchTree.add(13);
+        binarySearchTree.add(70);
         binarySearchTree.add(20);
-        binarySearchTree.add(22);
-        binarySearchTree.add(33);
-        binarySearchTree.add(19);
-        binarySearchTree.add(4);
-        binarySearchTree.add(11);
-        binarySearchTree.add(29);
-        binarySearchTree.add(31);
-        binarySearchTree.add(8);
-        binarySearchTree.add(21);
+        binarySearchTree.add(45);
+        binarySearchTree.add(65);
+        binarySearchTree.add(5);
+        binarySearchTree.add(80);
+        binarySearchTree.add(17);
+        binarySearchTree.add(25);
 
         System.out.println("Число узлов дерева: " + binarySearchTree.size());
 
