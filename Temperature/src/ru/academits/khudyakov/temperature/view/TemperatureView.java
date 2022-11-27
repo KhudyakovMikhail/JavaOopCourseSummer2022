e6ef1f89043784e26bec7cda03b467589c949018
Михаил <hudyakovmk@gmail.com>
2022-11-27 14:33:33 +0700

    Перенес view в View

--- Temperature/src/ru/academits/khudyakov/temperature/view/TemperatureView.java
+++ Temperature/src/ru/academits/khudyakov/temperature/view/TemperatureView.java
@@ -1,4 +1,78 @@
 package ru.academits.khudyakov.temperature.view;
 
+import ru.academits.khudyakov.temperature.model.*;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static ru.academits.khudyakov.temperature_main.TemperatureMain.isIDouble;
+
 public class TemperatureView {
+    public void getView() {
+        JFrame frame = new JFrame("Temperature");
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 150);
+        frame.setLocationRelativeTo(null);
+
+        JButton convertButton = new JButton("Convert");
+
+        JTextField inputTemperatureTextField = new JTextField();
+
+        JLabel outputTemperatureLabel = new JLabel("Result");
+
+        JPanel leftPanel = new JPanel();
+        leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
+
+        JPanel centerPanel = new JPanel();
+
+        JPanel rightPanel = new JPanel();
+        rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
+
+        JLabel inputScaleLabel = new JLabel("Input scale");
+        JComboBox<TemperatureScale> inputScaleComboBox = new JComboBox<>();
+        inputScaleComboBox.addItem(TemperatureScale.Celsius);
+        inputScaleComboBox.addItem(TemperatureScale.Kelvin);
+        inputScaleComboBox.addItem(TemperatureScale.Fahrenheit);
+
+        JLabel outputScaleLabel = new JLabel("Output scale");
+        JComboBox<TemperatureScale> outputScaleComboBox = new JComboBox<>();
+        outputScaleComboBox.addItem(TemperatureScale.Celsius);
+        outputScaleComboBox.addItem(TemperatureScale.Kelvin);
+        outputScaleComboBox.addItem(TemperatureScale.Fahrenheit);
+
+        frame.add(leftPanel, BorderLayout.LINE_START);
+        leftPanel.add(inputScaleLabel);
+        leftPanel.add(inputScaleComboBox);
+
+        frame.add(centerPanel, BorderLayout.CENTER);
+        centerPanel.add(convertButton);
+
+        frame.add(outputTemperatureLabel, BorderLayout.PAGE_END);
+
+        frame.add(rightPanel, BorderLayout.LINE_END);
+        rightPanel.add(outputScaleLabel);
+        rightPanel.add(outputScaleComboBox);
+
+        frame.add(inputTemperatureTextField, BorderLayout.PAGE_START);
+
+        convertButton.addActionListener(e -> {
+            TemperatureConverter temperatureConverter;
+
+            if (!isIDouble(inputTemperatureTextField.getText())) {
+                JOptionPane.showMessageDialog(new JFrame(), "To convert the temperature you have to enter a number");
+            }
+
+            if (inputScaleComboBox.getSelectedItem() == TemperatureScale.Celsius) {
+                temperatureConverter = new CelsiusConverter();
+            } else if (inputScaleComboBox.getSelectedItem() == TemperatureScale.Kelvin) {
+                temperatureConverter = new KelvinConverter();
+            } else {
+                temperatureConverter = new FahrenheitConverter();
+            }
+
+            outputTemperatureLabel.setText(String.valueOf(temperatureConverter.convert(Double.parseDouble(inputTemperatureTextField.getText()),
+                    (TemperatureScale) outputScaleComboBox.getSelectedItem())));
+        });
+    }
 }
--- Temperature/src/ru/academits/khudyakov/temperature_main/TemperatureMain.java
+++ Temperature/src/ru/academits/khudyakov/temperature_main/TemperatureMain.java
@@ -1,6 +1,7 @@
 package ru.academits.khudyakov.temperature_main;
 
 import ru.academits.khudyakov.temperature.model.*;
+import ru.academits.khudyakov.temperature.view.TemperatureView;
 
 import javax.swing.*;
 import java.awt.*;
@@ -18,56 +19,8 @@ public class TemperatureMain {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Temperature");
-            frame.setVisible(true);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(600, 400);
-            frame.setLocationRelativeTo(null);
-
-            JButton convertButton = new JButton("Convert");
-
-            JTextField inputTemperatureTextField = new JTextField();
-
-            JLabel outputTemperatureLabel = new JLabel("Result");
-
-            JLabel inputScaleLabel = new JLabel("Input scale");
-            JComboBox<TemperatureScale> inputScaleComboBox = new JComboBox<>();
-            inputScaleComboBox.addItem(TemperatureScale.Celsius);
-            inputScaleComboBox.addItem(TemperatureScale.Kelvin);
-            inputScaleComboBox.addItem(TemperatureScale.Fahrenheit);
-
-            JLabel outputScaleLabel = new JLabel("Output scale");
-            JComboBox<TemperatureScale> outputScaleComboBox = new JComboBox<>();
-            outputScaleComboBox.addItem(TemperatureScale.Celsius);
-            outputScaleComboBox.addItem(TemperatureScale.Kelvin);
-            outputScaleComboBox.addItem(TemperatureScale.Fahrenheit);
-
-            frame.add(inputScaleLabel, BorderLayout.LINE_START);
-            frame.add(inputScaleComboBox, BorderLayout.LINE_START);
-            frame.add(convertButton, BorderLayout.CENTER);
-            frame.add(outputTemperatureLabel, BorderLayout.PAGE_END);
-            frame.add(outputScaleLabel, BorderLayout.LINE_END);
-            frame.add(outputScaleComboBox, BorderLayout.LINE_END);
-            frame.add(inputTemperatureTextField, BorderLayout.PAGE_START);
-
-            convertButton.addActionListener(e -> {
-                TemperatureConverter temperatureConverter;
-
-                if (!isIDouble(inputTemperatureTextField.getText())) {
-                    JOptionPane.showMessageDialog(new JFrame(), "To convert the temperature you have to enter a number");
-                }
-
-                if (inputScaleComboBox.getSelectedItem() == TemperatureScale.Celsius) {
-                    temperatureConverter = new CelsiusConverter();
-                } else if (inputScaleComboBox.getSelectedItem() == TemperatureScale.Kelvin) {
-                    temperatureConverter = new KelvinConverter();
-                } else {
-                    temperatureConverter = new FahrenheitConverter();
-                }
-
-                outputTemperatureLabel.setText(String.valueOf(temperatureConverter.convert(Double.parseDouble(inputTemperatureTextField.getText()),
-                        (TemperatureScale) outputScaleComboBox.getSelectedItem())));
-            });
+            TemperatureView view = new TemperatureView();
+            view.getView();
         });
     }
 }
